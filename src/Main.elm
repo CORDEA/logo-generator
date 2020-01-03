53cee0c3407d25fe1746aa02bd34b16d9e5ea47f
Yoshihiro Tanaka <contact@cordea.jp>
2020-01-03 13:21:06 +0900

    update

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,40 @@
 module Main exposing (main)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
+-- MAIN
+
 main =
+    Browser.sandbox {
+        init = init,
+        update = update,
+        view = view
+    }
+
+-- MODEL
+
+type alias Model = { }
+
+init : Model
+init = { }
+
+-- UPDATE
+
+type Msg
+    = Message
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Message ->
+            { }
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
     div []
     [
         object [ type_ "image/svg+xml", attribute "data" "src/images/logo.svg" ] []
